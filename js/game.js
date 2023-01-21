5b2da00cd3f12ece872971e4d539fa4b8a4e767b
Alex <alex.isaguilera@hotmail.com>
2023-01-21 13:08:04 +0100

    working in player movement

--- js/game.js
+++ js/game.js
@@ -4,18 +4,22 @@ class Game {
     this.myCatImage
     this.background = new Background()
     this.backgroundImage
+    this.player = new Player()
+    this.playerImage
 
 }
 
 preload() {
     this.backgroundImage = loadImage("../assets/backgrounds/park.png")
     this.myCatImage = loadImage("../assets/cats/cat_white-32x48.png")
+    this.playerImage = loadImage("../assets/player/alex-front.png")
     
 }
 
 draw() {
     this.background.draw()
     this.myCat.draw()
+    this.player.draw()
 }
 
 }
--- js/player.js
+++ js/player.js
@@ -1,14 +1,41 @@
 class Player {
     constructor() {
-    this.width = 100
-    this.height = 100
-    this.x = 0
-    this.y = 0
+    this.width = 110
+    this.height = 180
+    this.x = 800
+    this.y = 300
 }
 
 draw() {
+    image(game.playerImage, this.x,this.y, this.width, this.height)
 
+if (keyIsDown(UP_ARROW)) {
+        if (this.y > 0) this.moveUp()
 }
 
+if (keyIsDown(DOWN_ARROW)) {
+    if (this.y < 600-this.height) this.moveDown()
+}
+
+if (keyIsDown(LEFT_ARROW)) {
+    if (this.x > 0) this.moveLeft()
+}
+
+if (keyIsDown(RIGHT_ARROW)) {
+    if (this.x < 1000-this.width) this.moveRight()
+}
+}
+
+moveUp(){
+    this.y -= 5}
+
+moveDown(){
+    this.y += 5}
+
+moveLeft(){
+    this.x -= 5}    
+
+moveRight(){
+    this.x += 5}
 
 }
